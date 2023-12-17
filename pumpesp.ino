7187df37513181d6c05852248409d41a106953ba
Christina Zhang <123713200+christinaz21@users.noreply.github.com>
2023-12-16 21:57:27 -0500

    Add files via upload

--- pumpesp.ino
+++ pumpesp.ino
@@ -1,4 +1,5 @@
 #define DAC_CH1 25
+int pwm = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,25 +8,23 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  dacWrite(DAC_CH1, 0);
-//  Serial.println("DAC Value 0");
-//  delay(3000);
-// 
-//  dacWrite(DAC_CH1, 64);
-//  Serial.println("DAC Value 64");
-//  delay(3000);
-// 
-//  dacWrite(DAC_CH1, 128);
-//  Serial.println("DAC Value 128");
-//  delay(3000);
-// 
-//  dacWrite(DAC_CH1, 192);
-//  Serial.println("DAC Value 192");
-//  delay(3000);
+  int inputFlowRate;
+  int voltage;
+
+  if (Serial.available()){
+    inputFlowRate = Serial.read();
+    voltage = 2 * inputFlowRate;
+    if (voltage >= 3.3) {
+      pwm = 255;
+    }
+    else {
+      pwm = voltage * 255 / 3.3;
+    }
+  }
+
+  dacWrite(DAC_CH1, pwm);
  
-  dacWrite(DAC_CH1, 255);
-//  Serial.println("DAC Value 255");
-//  delay(3000);
+ 
+
 
 }
